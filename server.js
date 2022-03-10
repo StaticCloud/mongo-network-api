962131cd6cadded4c5214e2c2ec9cf5afe40d70d
StaticCloud <diegomarrs@gmail.com>
2022-03-10 14:51:48 -0800

    added mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+// require express and mongoose
 const express = require('express');
+const mongoose = require('mongoose');
+
+// create express application
 const app = express();
+
+// JSON parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => { res.json() })
+// connect to mongodb database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+mongoose.set('debug', true);
+
+// connect express
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
